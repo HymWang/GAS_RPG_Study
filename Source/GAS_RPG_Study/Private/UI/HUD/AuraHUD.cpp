9c33a585cbaa334a38e4c490e9b8db1a00f773dd
Hyman <920888420@qq.com>
2025-09-29 17:34:11 +0800

    Add the UI class , UI Controller ,HUD Class

---
+++ Source/GAS_RPG_Study/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (OverlayWidgetClass)
+	{
+		UUserWidget* Widget = CreateWidget<UAuraUserWidget>(GetWorld(),OverlayWidgetClass);
+		Widget->AddToViewport();
+		
+	}
+}
